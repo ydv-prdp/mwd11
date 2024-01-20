71552615b7a3f5284a3659d5628e827c3fc2568d
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-20 19:41:30 +0530

    added loader

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var tl = gsap.timeline()
+function time(){
+    var tick = 0
+    setInterval(function(){
+        tick = tick +  Math.floor(Math.random()*10)
+        if(tick<100){
+            document.querySelector("#loader h1").innerHTML = `${tick}%`;
+        }
+
+    },150)
+}
+
+time()
+tl.to("#loader h1",{ 
+    delay:0.5,
+    duration:1,
+    onStart:time()
+})
+tl.to("#loader",{
+    top:"-100vh",
+    delay:0.5,
+    duration:1.5
+})
+
 gsap.to("#page1 h1",{
     transform:"translateX(-100%)",
     fontWeight:"10",
